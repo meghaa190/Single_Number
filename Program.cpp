badccbc20099d7924f7ec9fae9c163b6a6edb9bf
Megha <92908549+meghaa19@users.noreply.github.com>
2022-10-17 09:58:54 +0530

    Update Program.cpp

--- Program.cpp
+++ Program.cpp
@@ -2,31 +2,25 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int ans=0,i=0;
-        while(i<nums.size())
+        int ans=nums[0],i=0;
+        int n=nums.size();
+        while(i<n && n>1)
         {
             if(nums[i]==nums[i+1])
             {
                 i+=2;
-                //flag=0;
                 if(i==nums.size()-1)
                 {
                     ans=nums[i];
                     break;
-                }
-                
+                }      
             }
             else
             {
                 ans=nums[i];
                 break;
             }
-           
-                //ans= nums[i];
-            
         }
-        
-       
         return ans;
     }
 };
